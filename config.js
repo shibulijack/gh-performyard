3e1135e52d118c41fd06089334fc0c59145dc690
Shibu Lijack <shibu.lijack@freshworks.com>
2020-01-23 00:14:05 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  GITHUB_AUTH_TOKEN: 'YOUR_TOKEN_HERE',
+  GITHUB_ORG: 'freshdesk',
+  GITHUB_REPO: 'nucleus',
+  GITHUB_USER: 'shibulijack-fd',
+  PR_BASE_BRANCH: 'master',
+  PR_STATE: 'closed'
+}
